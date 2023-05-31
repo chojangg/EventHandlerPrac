b2b67b62f2a11539a8d27a24097637e8c4c7966f
chojangg <s2115@e-mirim.hs.kr>
2023-05-31 11:48:34 +0900

    버튼2개 추가

--- EventHandlerPrac/Form1.Designer.cs
+++ EventHandlerPrac/Form1.Designer.cs
@@ -36,7 +36,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(395, 197);
+            this.button1.Size = new System.Drawing.Size(410, 197);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -45,7 +45,7 @@
             // 
             this.button2.Location = new System.Drawing.Point(12, 221);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(394, 212);
+            this.button2.Size = new System.Drawing.Size(410, 212);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
