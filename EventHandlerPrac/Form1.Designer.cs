ebfa79f615c4072f519f73d212b8fd0a13947dfd
chojangg <s2115@e-mirim.hs.kr>
2023-05-31 12:08:48 +0900

    버튼2 이벤트핸들러 연결

--- EventHandlerPrac/Form1.Designer.cs
+++ EventHandlerPrac/Form1.Designer.cs
@@ -36,25 +36,27 @@
             // 
             this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(410, 197);
+            this.button1.Size = new System.Drawing.Size(395, 189);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 221);
+            this.button2.Location = new System.Drawing.Point(12, 208);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(410, 212);
+            this.button2.Size = new System.Drawing.Size(395, 185);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(434, 450);
+            this.ClientSize = new System.Drawing.Size(420, 409);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -69,4 +71,3 @@
         private System.Windows.Forms.Button button2;
     }
 }
-
